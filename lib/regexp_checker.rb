fe9d06a0fc25e4a26c3fde945b6ecd93d0bacbcb
masaya-kawamura <lucky.number0128@gmail.com>
2021-09-04 01:18:31 +0900

    [Finish]Regexp checker program

---
+++ lib/regexp_checker.rb
@@ -0,0 +1,21 @@
+# 正規表現チェッカープログラム
+
+print 'Text?: '
+text = gets.chomp
+
+# 例外処理を組み込んで再入力を可能にする
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e
+  puts "Invalid pattern: #{e.message}"
+  retry
+end
+
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched: #{matches.join(',')}"
+else
+  puts "Nothing matched."
+end
