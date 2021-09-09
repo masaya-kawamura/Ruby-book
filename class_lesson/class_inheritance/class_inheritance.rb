e6007efd9792cc3fbd5d80be30ee38f3bc93bff7
masaya-kawamura <lucky.number0128@gmail.com>
2021-09-09 15:36:56 +0900

    [fix]class_instance

--- class_lesson/class_inheritance/class_inheritance.rb
+++ class_lesson/class_inheritance/class_inheritance.rb
@@ -39,3 +39,14 @@ end
 puts Foo.hello #=> hello
 puts Bar.hello #=> hello
 
+class Foo
+  def self.hello
+    'hello'
+  end
+end
+
+class Bar < Foo
+end
+
+puts Foo.hello
+puts Bar.hello
---
+++ yield_proc_lesson/to_proc_method.rb
@@ -0,0 +1,72 @@
+# puts "求めたい数字を入力して下さい"
+# puts "１つ目の数字"
+# a = gets.to_i
+# puts "２つ目の数字"
+# b = gets.to_i
+# puts "３つ目の数字"
+# c = gets.to_i
+# puts "何番目の数字を求めますか？"
+# t = gets.to_i
+
+# n = 0
+# while n < (t - 3)
+#   d = a + b + c
+#   a = b
+#   b = c
+#   c = d
+#   n += 1
+# end
+
+# puts "#{t}番目の数は#{c}です"
+
+# n = 1
+# a = []
+# while n <= 30000000 do
+#   if 1234567890 % n == 0
+#     a << n
+#   end
+#   n += 1
+# end
+
+# puts a.sum
+
+# n = 1
+# a = []
+
+# while n <= 30000 do
+#   if n % 3 == 0
+#     a << n
+#   elsif n.to_s.include?('3')
+#     a << n
+#   end
+#   n += 1
+# end
+
+# puts a.sum
+
+# num = []
+# sum = 0
+# count = 0
+# while sum < 10
+#   count += 1
+#   reciprocal = 1.0/count
+#   num << reciprocal
+#   sum = num.sum
+# end
+# puts num.length
+
+# a = 205
+# b = 82
+# c = 30
+# d = []
+
+# 30.times do
+#   d << a
+#   a += 205
+# end
+
+# 40.
+
+# puts d.uniq.count
+
+p 30 * 40 * 30
